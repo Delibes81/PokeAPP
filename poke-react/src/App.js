992cd9b4367ebbd67ff21f6bd2975266cbbea284
Delibes <delibes.ats@gmail.com>
2024-09-03 21:27:27 -0600

    cambio a componente RandomPokemon

--- poke-react/src/App.js
+++ poke-react/src/App.js
@@ -1,36 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
+import RandomPokemon from './components/RandomPokemon';
 
-const fetchData = async (id) => {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-  const data = await response.json();
-  return data;
-}
 
 function App() {
-  const [pokemon, setPokemon] = useState(null);
 
-  const getRandomPokemon = async () => {
-    const randomId = Math.floor(Math.random() * 150) + 1;
-    const data = await fetchData(randomId);
-    setPokemon(data);
-  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <button onClick={getRandomPokemon}>Get Random Pokemon</button>
-        {pokemon && (
-          <div>
-            <h2>{pokemon.name}</h2>
-            <img 
-            src={pokemon.sprites.front_default} 
-            alt={pokemon.name}
-            style={{ width: 200, height: 200, backgroundColor: '#d6d6d6', borderRadius: 100 }}
-            />
-          </div>
-        )}
-      </header>
+      <RandomPokemon />
     </div>
   );
 }
