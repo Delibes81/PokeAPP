c4b3fd874d73a8d6f6765a741d979778ccab1ab4
Delibes81 <164214236+Delibes81@users.noreply.github.com>
2024-09-09 20:09:48 -0600

    Delete poke-react directory

--- poke-react/src/components/FetchData.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const fetchData = async (id) => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    const data = await response.json();
-    return data;
-  }
-
-  export default fetchData;
